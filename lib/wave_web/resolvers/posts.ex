437bb54180bf9e705404a564b0928bc627e9cec5
Thomas Johnson <thomasjohnson@Thomass-iMac.local>
2018-12-15 21:56:22 -0500

    created photo resolver

--- lib/wave_web/resolvers/posts.ex
+++ lib/wave_web/resolvers/posts.ex
@@ -2,4 +2,8 @@ defmodule WaveWeb.Resolvers.Posts do
   def photos(_, _, _) do
     {:ok, Wave.Posts.list_photos()}
   end
+
+  def photo(_, %{id: id}, _) do
+    {:ok, Wave.Posts.get_photo!(id)}
+  end
 end
--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -11,9 +11,11 @@ photos_list = [
   "https://res.cloudinary.com/bracket-factory/image/upload/v1532454233/saga/pexels-photo-577585.jpg",
   "https://res.cloudinary.com/bracket-factory/image/upload/v1532452456/saga/delicate-arch-night-stars-landscape.jpg",
   "https://res.cloudinary.com/bracket-factory/image/upload/v1532452456/saga/industry-sunrise.jpg",
-  "https://res.cloudinary.com/bracket-factory/image/upload/v1528082073/saga/abstract_V04.jpg"
+  "https://res.cloudinary.com/bracket-factory/image/upload/v1528082073/saga/abstract_V04.jpg",
+  "https://freestocks.org/fs/wp-content/uploads/2016/08/timber-800x533.jpg"
 ]
 
+#   Photos
 for idx <- 0..mock_photos do
   photo = %{
     image_url: Enum.at(photos_list, idx),
