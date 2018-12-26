97449bfe77e88b9fdae7048528bd6d0c1a269938
Thomas Johnson <thomasjohnson@Thomass-iMac.local>
2018-12-26 16:48:42 -0500

    created Accounts context

---
+++ priv/repo/migrations/20181226214605_create_users.exs
@@ -0,0 +1,20 @@
+defmodule Wave.Repo.Migrations.CreateUsers do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add(:username, :string)
+      add(:avatar, :string)
+      add(:facebook_id, :string)
+      add(:email, :string)
+      add(:first_name, :string)
+      add(:last_name, :string)
+
+      timestamps()
+    end
+
+    # unique -> users(table name) -> facebook_id
+    create(unique_index(:users, [:email]))
+    create_unique_index(:users, [:facebook_id])
+  end
+end
