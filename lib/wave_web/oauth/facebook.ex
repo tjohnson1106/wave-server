774ace39f46725812c85f9378ca1ba963e58ea6f
Thomas Johnson <thomasjohnson@Thomass-iMac.local>
2018-12-26 17:33:44 -0500

    created normalization method for data returned via facebook user token

--- lib/wave_web/oauth/facebook.ex
+++ lib/wave_web/oauth/facebook.ex
@@ -3,11 +3,32 @@ defmodule WaveWeb.Oauth.Facebook do
 
   def get_info(token) do
     token
+    # have user first argument
     |> get_user
+    # receive user, picture data
+    |> get_user_profile_picture("small", token)
+    |> normalize
   end
 
   defp get_user(token) do
     {:ok, user} = Facebook.me([fields: @fields], token)
     user
+  end
+
+  defp get_user_profile_picture(user, size, token) do
+    {:ok, data} = Facebook.picture(user["id"], size, token)
+    [user, data]
+  end
+
+  # normalize argument is a list
+  defp normalize([user, picture_data]) do
+    %{
+      # facebook id -> user
+      facebook_id: user["id"],
+      avatar: picture_data["data"]["url"],
+      first_name: user["first_name"],
+      last_name: user["last_name"],
+      email: user["email"]
+    }
   end
 end
